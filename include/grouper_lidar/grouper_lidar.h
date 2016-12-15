7f6708cc63a0aafcd65f590697254ba414bdacf6
Ph.X <pigheadx@gmail.com>
2016-12-15 22:11:28 +0800

    WIP

--- include/grouper_lidar/grouper_lidar.h
+++ include/grouper_lidar/grouper_lidar.h
@@ -12,6 +12,14 @@
 //#ifndef PI
 #define PI 3.14159
 //#endif
+#define MOTOR_BOARD_CW  -3.14159
+#define MOTOR_BOARD_CCW -1.60761
+#define MOTOR_TOLERANT 3
+#define SCAN_LINES 200
+#define LIDAR_POINTS 1081
+#define OUTPUT_ROW 200
+#define OUTPUT_COL 200
+#define LIDAR_STEP 2
 
 namespace grouper_lidar
 {
@@ -31,16 +39,34 @@ class GrouperLidar
   ros::ServiceClient motor_control_;
   ros::ServiceServer grouper_depthmap_control_;
   ros::Publisher grouper_depthmap_;
+  ros::Publisher grouper_intensity_;
   ros::Publisher grouper_pointcloud_;
   ros::Subscriber motor_state_;
+  ros::Subscriber lidar_scan_;
   
   std:string motor_name_;
   GROUPER_LIDAR_STATE operating_state_;
 
   dynamixel_control:MotorControl motor_srv_;
 
-  float motor_goal_position_;
-  float motor_present_position_;
+  double motor_goal_position_;
+  double motor_present_position_;
+  
+  float lidar_ranges_[];
+  float lidar_intensities_[];
+  
+  float ranges_matrix_[SCAN_LINES][LIDAR_POINTS];
+  float intensities_matrix_[SCAN_LINES][LIDAR_POINTS];
+
+  uint16_t ranges_img[OUTPUT_ROW][OUTPUT_COL];
+  uint16_t intensities_img[OUTPUT_ROW][OUTPUT_COL];
+
+  uint32_t msg_seq;
+
+  float scan_position_matrix_[SCAN_LINES];
+  int present_scan_line_;
+  
+  
  public:
   GrouperLidar();
   ~GrouperLidar();
@@ -48,10 +74,14 @@ class GrouperLidar
  private:
   void initGrouperLidar(void);
   void resetMotorPosition(void);
+  void moveMotor(double value);
+  bool motorReachGoal();
+  void LogLidar();
+  void output1Frame();
   bool grouperLidarControlCallback(grouper_lidar::GrouperLidarControl::Request  &req
                                    grouper_lidar::GrouperLidarControl::Response &res);
   void motorStateCallback(const dynamixel_control::MotorState::ConstPtr& msg);
-  bool motorReachGoal();
+  void lidarScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
 }
 }
 
--- src/grouper_lidar.cpp
+++ src/grouper_lidar.cpp
@@ -7,18 +7,7 @@ GrouperLidar::GrouperLidar()
      motor_name_("")
 {
   nh_priv_.param<std::string>("motor_name_", motor_name_, "radar_tilt");
-  
-  motor_control_ = nh_.serviceClient<dynamixel_control:MotorControl>("/dynamixel_control/motor_control");
-  grouper_depthmap_control_ = nh_.advertiseService("/grouper_lidar/capture_control", &GrouperLidar::grouperLidarControlCallback, this);
-//  grouper_depthmap_ = nh_.advertise<senser_msgs::Image>("/grouper_lidar/depthmap_image", 10);
-//  depthmap_image output
-//  grouper_pointcloud_ = nh.advertise<sensor_msgs::PointCloud2>( "/grouper_lidar/point_cloud", 10);
-//  pc output;
-  motor_state_ = nh_.subscribe("/dynamixel_control/motor_state", 10, chatterCallback, this);
-  operating_state_ = IDLE;
-  motor_srv_.motor_name = "motor_name_";
-  motor_srv_.control_type = "position";
-  motor_srv_.unit = "rad"
+  initGrouperLidar();
 }
 
 GrouperLidar::~GrouperLidar()
@@ -34,32 +23,203 @@ bool GrouperLidar::grouperLidarLoop(void)
       
     break;
     case INIT:
-      motor_srv_.value = -PI;
-      if (motor_control_.call(motor_srv_))
+      moveMotor(MOTOR_BOARD_CCW)
+      if (motorReachGoal())
       {
-        ROS_DEBUG("motor to %f", motor_srv_.response.value);
+        present_scan_line_ = 0;
+        operating_state_=CW;
+      }
+    break;
+    case CW:
+      if(motorReachGoal())
+      {
+        LogLidar();
+        if(present_scan_line_>=SCAN_LINES-1)
+        {
+          output1Frame();
+          operating_state_=CCW;
+        }
+        else
+        {
+          present_scan_line_++;
+          moveMotor(MOTOR_BOARD_CCW-(present_scan_line_*(MOTOR_BOARD_CCW-MOTOR_BOARD_CW)/(SCAN_LINES-1)));
+        }
       }
       else
       {
-        ROS_ERROR("Failed to call motor service");
+        ROS_ERROR("motor didn't reach goal");
       }
-      motor_goal_position_ = motor_srv_.response.value;
-      if (motorReachGoal())
-      {
-        operating_state_=CCW;
-      }
-        
-    break;
-    case CW:
-      
     break;
     case CCW:
-      
+      if(motorReachGoal())
+      {
+        LogLidar();
+        if(present_scan_line_<=0)
+        {
+          output1Frame();
+          operating_state_=CW;
+        }
+        else
+        {
+          present_scan_line_--;
+          moveMotor(MOTOR_BOARD_CCW-(present_scan_line_*(MOTOR_BOARD_CCW-MOTOR_BOARD_CW)/(SCAN_LINES-1)));
+        }
+      }
+      else
+      {
+        ROS_ERROR("motor didn't reach goal");
+      }
     break;
     default:
       
     break;
   }
+}
+
+void GrouperLidar::initGrouperLidar(void)
+{
+  motor_control_ = nh_.serviceClient<dynamixel_control:MotorControl>("/dynamixel_control/motor_control");
+
+  motor_state_ = nh_.subscribe("/dynamixel_control/motor_state", 10, motorStateCallback, this);
+  lidar_scan_ = nh_.subscribe("scan", 10, lidarScanCallback, this);
+
+  grouper_depthmap_ = nh_.advertise<senser_msgs::Image>("/grouper_lidar/depthmap_image", 10);
+  grouper_intensity_ = nh_.advertise<senser_msgs::Image>("/grouper_lidar/intensity_image", 10);
+  grouper_pointcloud_ = nh.advertise<sensor_msgs::PointCloud2>( "/grouper_lidar/point_cloud", 10);
+
+  grouper_depthmap_control_ = nh_.advertiseService("/grouper_lidar/capture_control", &GrouperLidar::grouperLidarControlCallback, this);
+
+  operating_state_ = IDLE;
+  motor_srv_.motor_name = motor_name_;
+  motor_srv_.control_type = "position";
+  motor_srv_.unit = "rad"
+  motor_goal_position_ = 0;
+  motor_present_position_ = 0;
+  memset(ranges_matrix_,0,sizeof(ranges_matrix_));
+  memset(intensities_matrix_,0,sizeof(intensities_matrix_));
+  memset(scan_position_matrix_,0,sizeof(scan_position_matrix_));
+  memset(ranges_img,0,sizeof(ranges_img));
+  memset(intensities_img,0,sizeof(intensities_img));
+  present_scan_line_ = 0;
+  msg_seq = 0;
+}
+
+void GrouperLidar::moveMotor(double value)
+{
+  motor_srv_.value = value;
+  if (motor_control_.call(motor_srv_))
+  {
+    ROS_DEBUG("motor to %f", motor_srv_.response.value);
+  }
+  else
+  {
+    ROS_ERROR("Failed to call motor service");
+  }
+  motor_goal_position_ = motor_srv_.response.value;
+}
+
+bool GrouperLidar::motorReachGoal()
+{
+  return (abs(motor_present_position_-motor_goal_position_) < MOTOR_TOLERANT);
+}
+
+void GrouperLidar::LogLidar()
+{
+  memcpy(ranges_matrix_[present_scan_line_],lidar_ranges_,LIDAR_POINTS*sizeof(float));
+  memcpy(intensities_matrix_[present_scan_line_],lidar_intensities_,LIDAR_POINTS*sizeof(float));
+  scan_position_matrix_[present_scan_line_] = motor_present_position_;
+  for(int i=0; i<OUTPUT_COL; i++)
+  {
+    ranges_img[present_scan_line_][i]=(int)ranges_matrix_[present_scan_line_][(LIDAR_POINTS/2)+LIDAR_STEP*(i-(OUTPUT_COL/2))]/60*65535;
+    intensities_img[present_scan_line_][i]=(int)intensities_matrix_[present_scan_line_][(LIDAR_POINTS/2)+LIDAR_STEP*(i-(OUTPUT_COL/2))]/60*65535;
+  }
+}
+
+void GrouperLidar::output1Frame()
+{
+  sensor_msgs::Image depthmap_msg;
+  depthmap_msg.header.seq = msg_seq;
+  depthmap_msg.header.stamp = ros::Time::now();
+  depthmap_msg.frame_id = "depthmap";
+  depthmap_msg.height = OUTPUT_ROW;
+  depthmap_msg.width = OUTPUT_COL;
+  depthmap_msg.encoding = "mono16";
+  depthmap_msg.is_bigendian = false;
+  depthmap_msg.step = OUTPUT_COL*sizeof(uint16_t);
+  depthmap_msg.data = ranges_img;
+  grouper_depthmap_.publish(depthmap_msg);
+
+  sensor_msgs::Image intensity_msg;  
+  intensity_msg.header.seq = msg_seq;
+  intensity_msg.header.stamp = ros::Time::now();
+  intensity_msg.frame_id = "intensity";
+  intensity_msg.height = OUTPUT_ROW;
+  intensity_msg.width = OUTPUT_COL;
+  intensity_msg.encoding = "mono16";
+  intensity_msg.is_bigendian = false;
+  intensity_msg.step = OUTPUT_COL*sizeof(uint16_t);
+  intensity_msg.data = intensities_img;
+  grouper_intensity_.publish(intensity_msg);
+
+  sensor_msgs::PointCloud2 pc_msg;
+  pcl::PointCloud< pcl::PointXYZI > pc;
+  pc.width  = LIDAR_POINTS;
+  pc.height = SCAN_LINES;
+  pc.is_dense = false;
+  pc.points.resize( pc.width * pc.height );
+  for(int i = 0; i < pc.height; ++i ) {
+    for(int j = 0; j < pc.width; ++j ) {
+      const int k = pc.width * i + j;
+      pc.points[k].x = ;
+      pc.points[k].y = ;
+      pc.points[k].z = ;
+      pc.points[k].intensity = intensities_matrix_[i][j];
+    }
+  }
+  pcl::toROSMsg(pc, pc_msg);
+  pc_msg.header.seq = msg_seq;
+  pc_msg.header.stamp = ros::Time::now();
+  pc_msg.frame_id = "pc";
+  grouper_pointcloud_.publish(pc_msg);
+
+  msg_seq++;
+}
+
+bool GrouperLidar::grouperLidarControlCallback(grouper_lidar::GrouperLidarControl::Request  &req
+                                               grouper_lidar::GrouperLidarControl::Response &res)
+{
+  res.ans=false;
+  if(req.command=="start")
+  {
+    operating_state_ = INIT;
+    res.ans = true;
+  }
+  if(req.command=="stop")
+  {
+    operating_state_ = IDLE;
+    res.ans = true;
+  }
+  if(req.command=="reset")
+  {
+    operating_state_ = IDLE;
+    res.ans = true;
+  }
+  return res.ans;
+}
+
+void GrouperLidar::motorStateCallback(const dynamixel_control::MotorState::ConstPtr& msg)
+{
+  if(msg.header.frame_id==motor_name_)
+  {
+    motor_present_position_=msg.present_position;
+    ROS_DEBUG("motor present %f", motor_present_position_);
+  }
+}
+
+void GrouperLidar::lidarScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+  lidar_ranges_ = msg.ranges;
+  lidar_intensities_ = msg.intensities;
 }
 
 int main(int argc, char **argv)
